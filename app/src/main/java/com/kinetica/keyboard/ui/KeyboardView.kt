2243b00c1e1acdec10916698db6b1397421cde45
freak07 <michalechner92@googlemail.com>
2026-09-07 08:09:44 +0200

    fix

--- app/src/main/java/com/kinetica/keyboard/ui/KeyboardView.kt
+++ app/src/main/java/com/kinetica/keyboard/ui/KeyboardView.kt
@@ -12,6 +12,7 @@ import android.os.SystemClock
 import android.util.AttributeSet
 import android.view.Choreographer
 import android.view.Gravity
+import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import android.view.View
 import android.widget.PopupWindow
@@ -212,19 +213,8 @@ class KeyboardView @JvmOverloads constructor(
         onStageUnits = { units, chars -> listener?.onStageDelete(units, chars) },
         onCommitStaged = { listener?.onCommitStagedDelete() },
         onRepeatVibrate = {
-        // Use a lighter haptic for rapid 50ms repeating
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            this.performHapticFeedback(
-                HapticFeedbackConstants.TEXT_HANDLE_MOVE,
-                HapticFeedbackConstants.FLAG_IGNORE_DEFAULT_INPUT
-            )
-        } else {
-            this.performHapticFeedback(
-                HapticFeedbackConstants.KEYBOARD_PRESS,
-                HapticFeedbackConstants.FLAG_IGNORE_DEFAULT_INPUT
-				)
-			}
-		}
+            listener?.onKeyPressFeedback()
+        }
     )
     private var spacePointer = -1
     private var backspacePointer = -1
